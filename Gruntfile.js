f12af843e40bd77d56d1545993d2dac0b9ee7501
Daniel Robinson <djr.self@gmail.com>
2015-04-25 17:06:54 -0400

    add js files to watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
       },
       watch: {
       options: { livereload: true },
-      files: ['index.html'],
+      files: ['index.html', 'js/*.js'],
       css: {
           files: '**/*.scss',
           tasks: ['sass']
