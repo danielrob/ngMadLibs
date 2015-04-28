b9a2f29d0d04e679ef43938485e0bda0c6bf98eb
Daniel Robinson <djr.self@gmail.com>
2015-04-28 15:37:47 -0400

    Add input/story toggling

--- js/ngMadLib.js
+++ js/ngMadLib.js
@@ -3,16 +3,28 @@ var ngMadLib = angular.module('ngMadLib', [])
 ngMadLib.controller('madLibsController', function($scope) {
 	var orderedWords = ['name','dirty','obnoxious','job','celeb','huge','task','skill','adjective']; 
 	var words = {
-							 'name':{'value':'', 'holder':'female name','size':11},
-							 'dirty':{'value':'', 'holder':'Dirty task', 'size':8},
-							 'obnoxious':{'value':'', 'holder':'Obnoxious celebrity', 'size':17},
-							 'job':{'value':'', 'holder':'Job title', 'size': 7},
-							 'celeb':{'value':'', 'holder':'Celebrity', 'size': 8},
-							 'huge':{'value':'', 'holder':'Huge number', 'size': 11},
-							 'task':{'value':'', 'holder':'Tedious task', 'size': 12},
-							 'skill':{'value':'', 'holder':'Useless skill', 'size': 11},
+							 'name':{'value':'', 'holder':'female name','size':11, 'type': 'text'},
+							 'dirty':{'value':'', 'holder':'Dirty task', 'size':8, 'type': 'text'},
+							 'obnoxious':{'value':'', 'holder':'Obnoxious celebrity', 'size':17, 'type': 'text'},
+							 'job':{'value':'', 'holder':'Job title', 'size': 7, 'type': 'text'},
+							 'celeb':{'value':'', 'holder':'Celebrity', 'size': 8, 'type': 'text'},
+							 'huge':{'value':'', 'holder':'Huge number', 'size': 7, 'type': 'number'},
+							 'task':{'value':'', 'holder':'Tedious task', 'size': 12, 'type': 'text'},
+							 'skill':{'value':'', 'holder':'Useless skill', 'size': 11, 'type': 'text'},
 							 'adjective':{'value':'', 'holder':'Adjective', 'size': 8}
 							};
+
 	$scope.orderedWords = orderedWords;
 	$scope.words = words;
+	$scope.tellStory = false;
+
+	$scope.emptyWordsAndAskForInput = function(){
+		for (var key in words) {
+			if (words.hasOwnProperty(key)) {
+				words[key].value = null;
+			}
+		}
+		$scope.tellStory = false
+	};
+
 });
