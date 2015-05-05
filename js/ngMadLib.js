ddbae59cf844af9f61fed492bc861d7d867b3886
Daniel Robinson <djr.self@gmail.com>
2015-05-05 16:50:00 -0400

    Use Angular Validation

--- js/ngMadLib.js
+++ js/ngMadLib.js
@@ -1,28 +1,53 @@
 var ngMadLib = angular.module('ngMadLib', [])
-	
+  
 ngMadLib.controller('madLibsController', function($scope) {
-	var words = {
-							 'name':'',
-							 'dirty':'',
-							 'obnoxious':'',
-							 'job':'',
-							 'celeb':'',
-							 'huge':'',
-							 'task':'',
-							 'skill':'',
-							 'adjective':'',
-							};
+  var words = {
+               'name':'',
+               'dirty':'',
+               'obnoxious':'',
+               'job':'',
+               'celeb':'',
+               'huge':'',
+               'task':'',
+               'skill':'',
+               'adjective':'',
+              };
 
-	$scope.words = words;
-	$scope.tellStory = false;
+  $scope.words = words;
+  $scope.tellStory = false;
 
-	$scope.emptyWordsAndAskForInput = function(){
-		for (var key in words) {
-			if (words.hasOwnProperty(key)) {
-				words[key] = null;
-			}
-		}
-		$scope.tellStory = false
-	};
+
+  $scope.submitWords = function(){
+    if (isEmpty($scope.wordInput.$error)){
+      $scope.tellStory = true;
+    };
+  };
+
+
+  $scope.reset = function(){ emptyWords(); askForInput(); };
+
+
+  function emptyWords(){
+    for (var key in words) {
+      if (words.hasOwnProperty(key)) {
+        words[key] = null;
+      }
+    }
+  };
+
+  function askForInput(){
+    $scope.tellStory = false
+    $scope.wordInput.$setPristine();
+    $scope.wordInput.$setUntouched();
+  };
+
+  function isEmpty(obj) {
+      for(var prop in obj) {
+          if(obj.hasOwnProperty(prop))
+              return false;
+      }
+
+      return true;
+    }
 
 });
